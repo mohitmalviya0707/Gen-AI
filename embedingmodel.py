ddfad8c4ad9d48a9dfcaf26d11f375d6459e07da
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-03-21 10:21:23 +0530

    Update embedingmodel.py

--- embedingmodel.py
+++ embedingmodel.py
@@ -1,7 +1,7 @@
 from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()   
 
 embeddings = OpenAIEmbeddings(
     model = 'text-embedding-3-large',
